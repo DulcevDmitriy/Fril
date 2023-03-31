a21dc82883532202e8d02b85365a4af3e2dcb751
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2023-03-31 20:20:08 +0300

    FrillService

--- src/main/java/ru/netology/services/FrilanserService.java
+++ src/main/java/ru/netology/services/FrilanserService.java
@@ -1,4 +1,22 @@
-package org.example;
+package ru.netology.services;
 
 public class FrilanserService {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int counter = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money = money - expenses;
+                money = money / 3;
+                counter++;
+            }
+            else {
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+
+        return counter;
+    }
 }
+
